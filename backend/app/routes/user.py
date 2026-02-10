e00c97a6ccb6803e515f78f97a0db73ff29c1bdc
KartikNaik205 <pkartiknaik@gmail.com>
2026-02-10 18:54:10 +0530

    Day 3: added users API and status responses

--- backend/app/routes/user.py
+++ backend/app/routes/user.py
@@ -1,11 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from app.schemas.user import UserCreate
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.post("/")
+@router.post("/", status_code=status.HTTP_201_CREATED)
 def create_user(user: UserCreate):
     return {
         "message": "User created successfully",
         "user": user
+    }
+
+@router.get("/", status_code=status.HTTP_200_OK)
+def get_users():
+    return {
+        "message": "Users fetched successfully",
+        "users": []
     }
